ddf1b4c02b07c40bce899b755654c9ea4d733546
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-09-24 18:26:39 -0300

    Add custom color palette to Tailwind config

---
+++ styles/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        menfa: {
+          orange: '#FF5B00',
+          violet: '#2D004B',
+          white:  '#FFFFFF',
+          carbon: '#1C1E21',
+        }
+      }
+    }
+  }
+}
